3fe39336aef084e15dfa05a53a2240fb70da5cb3
Collin Barney <127068052+cybarney@users.noreply.github.com>
2025-02-04 17:40:00 -0700

    Added Craig's chat screen
    Created button for chat screen as well as the chat screen itself.
    
    Changes to files:
    MainActivity.kt
    BottomNavHost.kt
    BottomNavItem.kt
    strings.xml(All versions)

--- app/src/main/java/com/greybox/projectmesh/navigation/BottomNavHost.kt
+++ app/src/main/java/com/greybox/projectmesh/navigation/BottomNavHost.kt
@@ -66,7 +66,7 @@ fun BottomNavigationBar(navController: NavHostController) {
                         launchSingleTop = true
                     }
                 },
-                
+
             )
         }
     }
